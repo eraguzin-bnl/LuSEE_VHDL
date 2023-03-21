b981f9068e681c27103702d83aa4ce267c88946b
Eric <eraguzin@bnl.gov>
2023-03-21 13:24:04 -0400

    Yet another toolchain touchup

--- simulation/setup.sh
+++ simulation/setup.sh
@@ -8,3 +8,4 @@ pip install -r requirements.txt
 echo "LuSEE --> Python packages installed"
 cp config_example.json config.json
 echo "LuSEE --> You now have your own 'config.json', don't forget to edit it with the path to this directory"
+echo -e "LuSEE --> Once done, run\npython3 simulate_plot.py config.json"
